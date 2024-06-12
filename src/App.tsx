4969188dcb2a01b4a1c7c8702f44802f6509fbbd
MarcinOstaszewski <marcinostaszewski@gmail.com>
2024-06-12 17:56:01 +0200

    innitial code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FestivalsList from './components/FestivalsList/FestivalsList';
+import Festival from './components/Festival/Festival';
+import TopMenu from './components/TopMenu/TopMenu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <TopMenu />
+        <Routes>
+          <Route path="/" Component={FestivalsList} />
+          <Route path="/festival/:id" Component={Festival} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
